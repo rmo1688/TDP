f6461c129d32cfa07a86c7e4a6a9babd7cc671db
rmo1688 <rmo1688@gmail.com>
2021-08-28 04:17:02 -0700

    Feat: include HK/CH indices

--- aastock_chartpx.py
+++ aastock_chartpx.py
@@ -1,3 +1,4 @@
+
 # -*- coding: utf-8 -*-
 """
 AAstock_chartpx
@@ -5,18 +6,44 @@ AAstock_chartpx
 """
 
 from bs4 import BeautifulSoup
-import requests, datetime, openpyxl, lxml
-import pandas as pd
+import requests
+import lxml
 import urllib3
+import datetime
+import openpyxl
+import pandas as pd
+
 
 def AA_Chart_Price(ticker):
-  #Generate URL
-  code, exchange, sec_type = ticker.split()
-  exchange = exchange.upper() if exchange.upper() != 'CH' else 'C1' if code[0] == '6' else 'C2'
-  exch_dict = {'HK':'HK','C1':'SH','C2':'SZ','US':'US'}
-  code = code.upper() + '.' + exch_dict[exchange]
+  #Convert Bloomberg ticker to AA Stock code to generate URL
+  ticker_ls = ticker.split()
+  code = ticker_ls[0].upper()
+  sec_type =ticker_ls[-1].lower()
+
+  INDEX_DICT = {
+              'HSI':'110000.HK',
+              'HSCEI':'110010.HK',
+              'HSTECH':'110078.HK',
+              'SHSZ300':'000300.SH',
+              }
+
+  EXCH_DICT = {'HK':'HK','C1':'SH','C2':'SZ','US':'US'}
+  URL_1 = 'chartdata1.internet.aastocks.com/servlet/iDataServlet/getdaily?id='
+  URL_2 = '&type=24&market=1&level=1&period=56&encoding=utf8'
+  
+  if sec_type == 'index':
+    code = INDEX_DICT[code]
+    exchange = 'hk' if code[0] == 'h' else 'ch'
+  elif len(ticker_ls) > 3:
+    print('options')#####################################################################################
+    pass
+  else:
+    exchange = ticker_ls[1]
+    exchange = exchange.upper() if exchange.upper() != 'CH' else 'C1' if code[0] == '6' else 'C2'
+    code = code + '.' + EXCH_DICT[exchange]
+
   us_ex = 'us' if exchange == 'US' else ''
-  chartdata1_url= 'http://' + us_ex + 'chartdata1.internet.aastocks.com/servlet/iDataServlet/getdaily?id=' + code + '&type=24&market=1&level=1&period=56&encoding=utf8'
+  chartdata1_url= 'http://' + us_ex + URL_1 + code + URL_2
   
   #Request Soup
   http = urllib3.PoolManager()
@@ -26,21 +53,24 @@ def AA_Chart_Price(ticker):
   
   #Extract Price from Soup
   todayy = datetime.datetime.today()
+  #todayy = datetime.datetime.fromisoformat("2021-08-27 10:10:10") #########TESTING############
   todayy = str(todayy.strftime('%m/%d/%Y'))
   price = soup_text.split(todayy)[1].split(';')[4]
+
   return price
 
 
 #dates
 in_date = datetime.datetime.today()
-yymmdd = in_date.strftime('%y%m%d')
-ddmmmyyyy = in_date.strftime('%d-%b-%Y')
+#in_date = datetime.datetime.fromisoformat("2021-08-27 10:10:10") #########TESTING############
+file_format_yymmdd = in_date.strftime('%y%m%d')
+loader_format_ddmmmyy = in_date.strftime('%d-%b-%Y')
 
 #Extract tickers from ticker file and store in dataframe
 portdf = pd.read_excel('port_tickers.xlsx') #need to leave header in excel
 tckr_ls = list(portdf[portdf.columns[0]])
 
-print('Getting prices for ' + ddmmmyyyy) #display
+print('Getting prices for ' + loader_format_ddmmmyy) #display
 
 px_dict = {} #stores prices and corresponding ticker
 for ticker in tckr_ls:
@@ -76,12 +106,12 @@ for hdr in tdp_hdr:
 ldr_dict[ldr_row] = ldr_hdr
 ldr_row += 1
 for i in px_dict:
-  ldr_dict[ldr_row] = [ddmmmyyyy,'BB_TCM',i,1,px_dict[i],'','','']
+  ldr_dict[ldr_row] = [loader_format_ddmmmyy,'BB_TCM',i,1,px_dict[i],'','','']
   ldr_row +=1
 ldr_df = pd.DataFrame.from_dict(ldr_dict,orient='index')
 
 #Generate PRICE Loader File
-filename = 'tdp_loader_PRICE_' + yymmdd + '.xlsx'
+filename = 'tdp_loader_PRICE_' + file_format_yymmdd + '.xlsx'
 print(filename)
 ldr_df.to_excel(filename,index=0,header=False)
 
