3be5745daecad2c9d5eb9e17ad60f92d4b4fb48c
rmo1688 <rmo1688@gmail.com>
2021-09-11 01:32:45 -0700

    Refactor TDP loader section

--- prices.py
+++ prices.py
@@ -186,58 +186,41 @@ for ticker in tqdm(tckr_ls):
     skipped_tickers[ticker] = ''
 
 #TDP Loader Definitions
-ldr_dict = {} # for loader content
-tdp_cred = [ #loader credentials
-            '#!CONNECT=HK053_RMO/HK053_RMO@PROD_HO3ORC08_FM.world',
-            '#!MAX_ERROR=1000',
-            '#!OPF=TDP_LOADER.import_price'
-            ]
+TDP_CRED_LS = [
+              '#!CONNECT=HK053_RMO/HK053_RMO@PROD_HO3ORC08_FM.world',
+              '#!MAX_ERROR=1000',
+              '#!OPF=TDP_LOADER.import_price',
+              ]
 #file type header chooser
-px_hdr = [ #headers for this type of loader
-          '#in_ladder_date',
-          'in_ident_type',
-          'in_ext_ident',
-          'in_value_spec',
-          'in_price',
-          'in_hilo_ind',
-          'in_price_ccy',
-          'in_notes',
-          ]
-fx_hdr = []
-delta_hdr = []
-
-ldr_hdr = px_hdr #picked which file/header to use
-ldr_col = len(ldr_hdr) #counter and defines the width of the file by # of columns
-ldr_row = 0 #counter
+TDP_HEADERS_DICT = {
+                    'px'    : [ #headers for this type of loader
+                              '#in_ladder_date',
+                              'in_ident_type',
+                              'in_ext_ident',
+                              'in_value_spec',
+                              'in_price',
+                              'in_hilo_ind',
+                              'in_price_ccy',
+                              'in_notes',
+                              ],
+                    'fx'    : [],
+                    'delta' : [],
+                  }
 
 #TDP Loader Construction
-ldr_dict = {}
-tdp_hdr = [
-          '#!CONNECT=HK053_RMO/HK053_RMO@PROD_HO3ORC08_FM.world',
-          '#!MAX_ERROR=1000',
-          '#!OPF=TDP_LOADER.import_price',
-          ]
-px_hdr = [
-          '#in_ladder_date',
-          'in_ident_type',
-          'in_ext_ident',
-          'in_value_spec',
-          'in_price',
-          'in_hilo_ind',
-          'in_price_ccy',
-          'in_notes'
-          ]
-ldr_row = 0
-for hdr in tdp_hdr:
-  ldr_list = [''] * len(px_hdr)
-  ldr_list[0] = hdr
-  ldr_dict[ldr_row] = ldr_list
-  ldr_row += 1
-ldr_dict[ldr_row] = ldr_hdr
-ldr_row += 1
+ldr_dict = {} # for loader content to be converted into pandas dataframe
+row = 0 # row counter in loader
+
+for line in TDP_CRED_LS: # this loop sets up the loader credentials into the dictionary to be converted into pandas df
+  ldr_list = [''] * len(TDP_HEADERS_DICT['px']) # sets number of blank cells and file width
+  ldr_list[0] = line # inserts tdp credentials into first column of row
+  ldr_dict[row] = ldr_list # add row to dataframe dict
+  row += 1
+ldr_dict[row] = TDP_HEADERS_DICT['px']
+row += 1
 for i in px_dict:
-  ldr_dict[ldr_row] = [loader_format_ddmmmyy,'BB_TCM',i,1,px_dict[i],'','','']
-  ldr_row +=1
+  ldr_dict[row] = [loader_format_ddmmmyy,'BB_TCM',i,1,px_dict[i],'','','']
+  row +=1
 ldr_df = pd.DataFrame.from_dict(ldr_dict,orient='index')
 
 print(filename := 'tdp_loader_PRICE_' + file_format_yymmdd)
